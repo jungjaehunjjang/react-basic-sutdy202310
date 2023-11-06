76a511cd6b2e9955a5de5323401612f8fe3d57b0
jaehun <josh9269@naver.com>
2023-11-06 10:59:32 +0900

    에러 상태 헨들링

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -4,11 +4,14 @@ import Card from '../UI/Card';
 import Button from '../UI/Button/Button';
 import ErrorModal from '../UI/Modal/ErrorModal';
 
-const AddUsers = () => {
+const AddUsers = ({ onAddUser }) => {
   const [userValue, setUserValue] = useState({
     userName: '',
     age: '',
   });
+
+  // 에러 상태 관리
+  const [error, setError] = useState(null);
 
   const userNameChangeHandler = (e) => {
     setUserValue((prevUserValue) => ({
@@ -28,11 +31,20 @@ const AddUsers = () => {
     e.preventDefault();
 
     if (userValue.userName.trim() === '' || userValue.age.trim() === '') {
+      setError({
+        title: '유호하지 않은 입력값',
+        message: '입력값은 공백으로 작성하면 안됩니다!',
+      });
       return;
     }
 
-    if (+userValue.age < 1) return;
-
+    if (+userValue.age < 1) {
+      setError({
+        title: '유효하지 않은 나이의 범위',
+        message: '나이는 1 이상의 숫자로 작성해 주세요!',
+      });
+      return;
+    }
     onAddUser(userValue);
 
     setUserValue({
@@ -44,10 +56,13 @@ const AddUsers = () => {
   return (
     // React.Fragment
     <>
-      <ErrorModal
-        title={'아무제목'}
-        message={'아무 메세지 입니다~~~'}
-      />
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={() => setError(null)}
+        />
+      )}
       <Card className={styles.input}>
         <form onSubmit={userSubmitHandler}>
           <label htmlFor='username'>이름</label>

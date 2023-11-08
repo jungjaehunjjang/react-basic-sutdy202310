b6535a35fb00ea3c4611299a0ad04f709c7102c7
jaehun <josh9269@naver.com>
2023-11-08 12:37:47 +0900

    useContext와 리팩토링

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -3,6 +3,8 @@ import React from 'react';
 // 로그인 상태 변수를 관리할 컨텍스트
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
 });
 
 export default AuthContext;
